1c43b53093d1b30bcfbf6f5def97a60295c2f13f
shiftsayan <sayan.chaudhry@gmail.com>
2023-02-13 23:26:11 -0500

    Change files

--- map-example.js
+++ map-example.js
@@ -1,11 +1,10 @@
-// After cloning this repository, rename this file to `map.js`.
-// You can modify the tags `personal`, `work`, and `todo` and add your own.
-// You can find the calendar IDs in the Integrate Calender section of calendar settings.
-// The calendar ID is the string preceding the `@group.calendar.google.com`.
-// Nothing stops from multiple tags mapping to the same calendar ID.
+// After cloning this repository, rename this file to `map.js`. You can modify the tags
+// `personal`, `work`, and `todo` and map them to your own calendar IDs. Multiple tags
+// can be mapped to the same calendar ID. The calendar IDs are the `data-value`
+// attributes of the calendar elements in the dropdown menu.
 
 map_tag_to_calendar_id = {
-    'personal': 'sample_calendar_id',
-    'work': 'another_sample_calendar_id',
-    'todo': 'third_sample_calendar_id'
-}
+  personal: "sample_calendar_id",
+  work: "another_sample_calendar_id",
+  todo: "third_sample_calendar_id",
+};
